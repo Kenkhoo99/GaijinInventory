8a2c4cdefbb89d5bc33b5880ad240df81a7909af
Ken <k.layken99@gmail.com>
2020-08-01 16:18:22 +0800

    Commit 5

--- app/src/main/java/com/example/gaijininventory/MainActivity.java
+++ app/src/main/java/com/example/gaijininventory/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.gaijininventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){}
 
+        EditText et = (EditText) findViewById(R.id.username);
+        EditText et1 = (EditText) findViewById(R.id.editTextTextPassword);
+        et.setHintTextColor(Color.WHITE);
+        et1.setHintTextColor(Color.WHITE);
 
+        button = findViewById(R.id.Login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStaffManagementMainMenu();
+            }
+        });
+    }
+
+    public void openStaffManagementMainMenu(){
+        Intent intent = new Intent(this,StaffManagementMainMenu.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/gaijininventory/StaffManagementMainMenu.java
@@ -0,0 +1,15 @@
+package com.example.gaijininventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffManagementMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_management_main_menu);
+        this.setTitle("Staff Management");
+    }
+}
